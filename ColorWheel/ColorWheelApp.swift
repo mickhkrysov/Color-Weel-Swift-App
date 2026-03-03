b8c3fe12d1dacca2a0d36642648420936ec96bcd
Mikhail Krysov <mickhkrysov@gmail.com>
2026-03-02 21:29:09 -0500

    Initial Commit

---
+++ ColorWheel/ColorWheelApp.swift
@@ -0,0 +1,32 @@
+//
+//  ColorWheelApp.swift
+//  ColorWheel
+//
+//  Created by Iaroslava Krysova on 02.03.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ColorWheelApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ColorWheelTests/ColorWheelTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColorWheelTests.swift
+//  ColorWheelTests
+//
+//  Created by Iaroslava Krysova on 02.03.2026.
+//
+
+import Testing
+@testable import ColorWheel
+
+struct ColorWheelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
